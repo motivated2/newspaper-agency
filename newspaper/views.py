dce4dd2d166421a0a415d390b2f9feb2ae38c71c
Ihor Bondarenko <igorb6766@gmail.com>
2024-03-06 12:50:43 +0200

    add views for index, RedactorListView, TopicListView, NewspaperListView

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from newspaper.models import Redactor, Topic, Newspaper
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_redactors = Redactor.objects.count()
+    num_topics = Topic.objects.count()
+    num_newspapers = Newspaper.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_topics": num_topics,
+        "num_newspapers": num_newspapers,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "newspaper/index.html", context=context)
+
+
+class RedactorListView(ListView):
+    model = Redactor
+
+
+class TopicListView(ListView):
+    model = Topic
+
+
+class NewspaperListView(ListView):
+    model = Newspaper
