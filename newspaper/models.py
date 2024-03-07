2636b6464834e013ed8702f72200491505c4984f
Ihor Bondarenko <igorb6766@gmail.com>
2024-03-07 11:36:59 +0200

    changed newspaper-topic relation

--- newspaper/models.py
+++ newspaper/models.py
@@ -21,7 +21,7 @@ class Newspaper(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     published_date = models.DateField(auto_now_add=True)
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    topics = models.ManyToManyField(Topic, related_name='newspapers')
     publishers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="newspapers")
 
     def __str__(self):
