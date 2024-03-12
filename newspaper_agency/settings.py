c50fb8701f6534ac0fa81d8c7e85faa3fa624221
Ihor Bondarenko <igorb6766@gmail.com>
2024-03-12 17:42:59 +0200

    remove insecure django secret key for deployment

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", os.getenv("SECRET_KEY"))
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
